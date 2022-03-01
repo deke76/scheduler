23105f53c8d9efaaf7a8b55c93604d69c1883892
Darcy <darcy.lauder@shaw.ca>
2022-03-01 08:09:19 -0700

    update spots working

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { getAppointmentsForDay } from "helpers/selector";
 import axios from "axios";
 
 export default function useApplicationData() {
@@ -17,14 +16,6 @@ export default function useApplicationData() {
   const setDay = day => setState({...state, day });
   const setAppointments = (appointments) => setState({...state, appointments});
 
-  const updateSpots = function(id) {
-    const dayDetails = [...state.days].filter(day => day.name === state.day)[0];
-    dayDetails.spots += 1;
-    const dayID =  dayDetails.id - 1;
-    console.log(dayID, dayDetails);
-    return { [dayID]: dayDetails }
-  }
-
   useEffect(() => {
     Promise.all([
       axios.get(daysURL),
@@ -35,19 +26,24 @@ export default function useApplicationData() {
     })
   }, []);
 
-  const bookInterview = function(id, interview) {
-    console.log(state.appointments[id]);
-    const dayDetails = state.days.filter(day => day.name === state.day)[0];
-    dayDetails.spots -= 1;
-    const dayID =  dayDetails.id - 1;
+  const addOrEditSpots = function(appointment) {
+    return (appointment === null) ? (spots => spots -= 1 ) : (spots => spots );
+  }
+  const deleteSpot = spots => spots += 1;
 
-    console.log(dayID);
-    console.log(dayDetails);
-    
-    const days = {
-      ...state.days,
-      [dayID]: dayDetails
-    };
+  const updateSpots = function(id, callBack) {
+    const dayDetails = [...state.days].filter(day => day.name === state.day)[0];
+    console.log(dayDetails === state.days[0]);
+    dayDetails.spots = (callBack(dayDetails.spots));
+    // const dayID =  dayDetails.id - 1;
+    // return { [dayID]: dayDetails }
+  }
+
+  const bookInterview = function(id, interview) {
+    // const days = {
+    //   ...state.days,
+    //   ...updateSpots(id, addOrEditSpots(state.appointments[id].interview))
+    // };
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -60,18 +56,18 @@ export default function useApplicationData() {
     return axios
       .put(`${appointmentsURL}/${id}`, appointment)
       .then((res) => {
-        if (res.status === 204) setAppointments(appointments);
+        if (res.status === 204) {
+          updateSpots(id, addOrEditSpots(state.appointments[id].interview));
+          setAppointments(appointments);  
+        }
       });
   }
 
   const cancelInterview = function(id) {
-    
-    const newDay = updateSpots(id);
-    console.log(newDay);
-    const days = {
-      ...state.days,
-      // [dayID]: dayDetails
-    };
+    // const days = {
+    //   ...state.days,
+    //   ...updateSpots(id, deleteSpot)
+    // };
     const appointment = {
     ...state.appointments[id],
     interview: null
@@ -81,13 +77,10 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    console.log(state);
-    console.log(state.days);
-    console.log(days);
-
     return axios
       .delete(`${appointmentsURL}/${id}`, appointments)
       .then((res) => {
+        updateSpots(id, deleteSpot);
         setAppointments(appointments);
       });
     }
