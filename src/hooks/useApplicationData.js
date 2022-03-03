07f14d2dce67ac39688313944b2a9635aee6ee18
Darcy <darcy.lauder@shaw.ca>
2022-03-03 04:18:14 -0700

    Centralized all import/exports & form functinality

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { URL, DAYS, APPTS, INTERVIEWERS } from '../constants'
 import axios from "axios";
 
-
 export default function useApplicationData() {
   const [state, setState] = useState({
     day: 'Monday',
